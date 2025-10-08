57690ed591e42ff7642427ee2cc873366130669a
sevianmsc <110935070+sevianmsc@users.noreply.github.com>
2025-10-08 12:27:02 -0300

    update
    se agrego la ruta para registrar un vehiculo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'vehicle-form', component: VehicleFormComponent },
   { path: '**', redirectTo: '' },
 ];
