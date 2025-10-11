47e4a9d171a8b6a3f14dcc40ed2221c89f3af6bd
sevianmsc <se.mena@duocuc.cl>
2025-10-11 20:49:58 -0300

    ts de seguimiento-mantenimiento
    ts default

---
+++ frontend/src/app/seguimiento-mantenimiento/seguimiento-mantenimiento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seguimiento-mantenimiento',
+  standalone: true,
+  imports: [],
+  templateUrl: './seguimiento-mantenimiento.component.html',
+  styleUrl: './seguimiento-mantenimiento.component.scss'
+})
+export class SeguimientoMantenimientoComponent {
+
+}
