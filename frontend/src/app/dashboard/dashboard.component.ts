bfde597309b54b92e0ee54a6fa4bf30202bb6c0a
sevianmsc <110935070+sevianmsc@users.noreply.github.com>
2025-10-11 02:20:36 -0300

    routerlink
    importar routerlink a ts

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink], 
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   currentUser: any = null;
