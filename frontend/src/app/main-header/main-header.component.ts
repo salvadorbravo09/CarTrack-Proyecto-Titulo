a0c98b78f8bb2a89599017327f0564ab70b89b4e
Salvador Bravo <sa.bravo0901@gmail.com>
2025-11-08 20:10:18 -0300

    agregar botón de administración en el encabezado

--- frontend/src/app/main-header/main-header.component.ts
+++ frontend/src/app/main-header/main-header.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-main-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './main-header.component.html',
   styleUrls: ['./main-header.component.scss']
 })
