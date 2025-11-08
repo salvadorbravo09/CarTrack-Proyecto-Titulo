0577bfe64c95b6491f36a1a8cbc87f7c0c25bf65
Salvador Bravo <sa.bravo0901@gmail.com>
2025-11-08 17:47:50 -0300

    agregar ruta para seguros vehiculares

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ import authRoutes from './authRoutes.js';
 import vehicleRoutes from './vehicleRoutes.js';
 import maintenanceRoutes from './maintenanceRoutes.js';
 import fuelRoutes from './fuelRoutes.js';
+import insuranceRoutes from './insuranceRoutes.js';
 
 const router = Router();
 
@@ -17,6 +18,9 @@ router.use('/maintenance', maintenanceRoutes);
 
 // Rutas de combustible
 router.use('/fuel', fuelRoutes);
+
+// Rutas de seguros vehiculares
+router.use('/insurances', insuranceRoutes);
 
 // TODO: Agregar más rutas después
 // router.use('/users', userRoutes);
