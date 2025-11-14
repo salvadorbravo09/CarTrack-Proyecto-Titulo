f4b9afca57860f4ff76d227671103229c68f562a
sevianmsc <se.mena@duocuc.cl>
2025-11-14 18:23:51 -0300

    fix: main footer inyectado a landing page

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/frontend/src/app/landing-page/landing-page.component.ts
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/frontend/src/app/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FeatureCardComponent } from '../feature-card/feature-card.component';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [FeatureCardComponent, HeaderComponent],
+  imports: [FeatureCardComponent, HeaderComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
